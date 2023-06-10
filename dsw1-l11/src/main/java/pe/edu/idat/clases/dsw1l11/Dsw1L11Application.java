1bf1d9774ef32037eec843bf4531247ebf4de8fc
Ethel <ethelcruz602@gmail.com>
2023-06-10 10:52:08 -0500

    segundo commit

---
+++ dsw1-l11/src/main/java/pe/edu/idat/clases/dsw1l11/Dsw1L11Application.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.clases.dsw1l11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsw1L11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsw1L11Application.class, args);
+	}
+
+}
---
+++ dsw1-l11/src/test/java/pe/edu/idat/clases/dsw1l11/Dsw1L11ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.clases.dsw1l11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsw1L11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
